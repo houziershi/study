2f61f85f52e55858042affdba7085f7f64885134
Jade-Shan <evokeralucard@gmail.com>
2012-05-24 09:57:56 +0800

    update python note

--- python/py/coreda/exdecimal.py
+++ python/py/coreda/exdecimal.py
@@ -5,3 +5,10 @@ import decimal
 d = decimal.Decimal('3.141')
 
 print d+1
+
+print decimal.Decimal('0.1') + decimal.Decimal('0.1') + decimal.Decimal('0.1') 
+print decimal.Decimal('1') / decimal.Decimal('7') 
+
+# set precision
+decimal.getcontext().prec = 4
+print decimal.Decimal('1') / decimal.Decimal('7') 
