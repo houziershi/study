9e238235cafd744fa2eb66c1a1107a887808c55c
Jade Shan <evokeralucard@gmail.com>
2012-07-22 19:29:05 +0800

    update
    update

--- python/py/expoo05.py
+++ python/py/expoo05.py
@@ -1,15 +1,12 @@
 # coding=utf-8
 
-class C1():
-	def __init__(self, value):
-		self.data = value
-	def __add__(self, other):
-		return C1(self.data + other)
-	def __mul__(self, other):
-		return C1(self.data * other)
+class CallBack:
+	def __init__(self,color):
+		self.color = color
+	def show(self):
+		print 'color: ' + self.color
 
-a = C1(5)
-b = C1(10)
+a = CallBack('red')
+b = CallBack('blue')
 
-print (a + 3).data           # result is 8
-print (b * 5).data           # result is 50
+
