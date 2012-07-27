8a7da1f38d18302d6eaaf0ca9974d70b343c135f
Jade Shan <evokeralucard@gmail.com>
2012-07-27 11:05:58 +0800

    update python study note
    update

---
+++ python/py/chap.oop/otherfile.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+import manynames
+
+X = 66
+
+print X             # 66: the global here
+
+print manynames.X   # 11: globals become attributes after imports
+
+manynames.f()       # 11: func f() print manyname.X
+manynames.g()       # 22: local in other file's function
+
+print manynames.C.X # 33: attribute of class in other module
+
+I = manynames.C()
+print I.X           # 33: still from class here
+
+I.m()
+print I.X           # 55: now from instance
