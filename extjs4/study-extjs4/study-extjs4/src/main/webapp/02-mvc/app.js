dd4761cbad88b7167756ea71e9d9f1688241d643
Jade Shan <evokeralucard@gmail.com>
2013-05-11 02:07:35 +0800

    update extjs

---
+++ extjs4/study-extjs4/study-extjs4/src/main/webapp/02-mvc/app.js
@@ -0,0 +1,14 @@
+Ext.application({
+	requires : [ 'Ext.container.Viewport' ],
+	name : 'AM',
+	appFolder : 'app',
+
+	controllers : [ 'Users' ],
+	
+	launch : function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: { xtype: 'userlist' }
+        });
+	}
+});
