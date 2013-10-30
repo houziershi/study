fe2c4a0957b2778187f9b62b49e777a8bfdbce1f
Jade Shan <evokeralucard@gmail.com>
2013-10-30 17:33:27 +0800

    snapshot2

--- notes/code/angular.js/quickview/js/service.js
+++ notes/code/angular.js/quickview/js/service.js
@@ -1,42 +1,61 @@
 var app = angular.module('myApp', []);
 
-angular.module('myApp.services', []).factory('githubService', ['$http', 
-	function($http) {
-		var doRequest = function(username, path) {
-			return $http({
-				method: 'JSONP',
-				url: 'https://api.github.com/users/' + username + '/' + path + '?callback=JSON_CALLBACK'
-			});
-		}
-		return {
-			events: function(username) {
-				return doRequest(username, 'events'); 
-			},
-		};
+app.factory('githubService1', ['$http', function($http) {
+	var doRequest = function(username, path) {
+		return $http({
+			method: 'JSONP',
+			url: 'https://api.github.com/users/' + username + '/' + path 
+				+ '?callback=JSON_CALLBACK'
+		});
+	}
+
+	return {
+		events: function(user) { return doRequest(user, 'events'); }
+	};
 }]);
 
-app.controller('ServiceController', ['$scope', '$timeout', 'githubService',
-	function($scope, $timeout, githubService) {
-		// $timeout service
+app.factory('githubService2', ['$http', function($http) {
+	var _username;
+
+	var doRequest = function(path) {
+		return $http({
+			method: 'JSONP',
+			url: 'https://api.github.com/users/' + _username + '/' + path 
+				+ '?callback=JSON_CALLBACK'
+		});
+	}
+
+	return {
+		events: function() { return doRequest('events'); },
+		setUsername: function(username) { _username = username; }
+	};
+}]);
+
+app.controller('ServiceController', [
+	'$scope', '$timeout', 'githubService1', 'githubService2',
+	function($scope, $timeout, githubService1, githubService2) {
 		var timeout;
-		// Watch for changes on the username property.
-		// If there is a change, run the function
 		$scope.$watch('username', function(newVal) {
 			if (newVal) {
-				if (timeout) {
-					$timeout.cancel(timeout);
-				}
+				if (timeout) $timeout.cancel(timeout);
 				timeout = $timeout(function() {
-					// uses the $http service to call the GitHub API
-					// and returns the resulting promise
-					githubService.events(newVal).success(function(data, status) {
-						// the success function wraps the response in data
-						// so we need to call data.data to fetch the raw data
+					githubService2.setUsername(newVal);
+					githubService2.events()
+					.success(function(data, status) {
 						$scope.events = data.data;
 					});
 				}, 350);
 			}
 		});
+
+		$scope.fetchEvents = function() {
+			$scope.auserEvents = null;
+			githubService1.events($scope.username).success(
+				function(data, status, headers) {
+					$scope.auserEvents = data.data;
+				}
+			);
+		}
 	}
 ]);
 
