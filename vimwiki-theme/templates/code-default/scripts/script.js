30399eb35e4a85c17de1f4f149aefec86c53c972
Jade Shan <evokeralucard@gmail.com>
2014-02-10 16:49:17 +0800

    update

--- vimwiki-theme/templates/code-default/scripts/script.js
+++ vimwiki-theme/templates/code-default/scripts/script.js
@@ -6,41 +6,39 @@ var hlCodePath = '../../vimwiki-theme/3rd-libs/hightlight-code/scripts/';
  */
 function path() {
 	var args = arguments, result = [];
-
-	for(var i = 0; i < args.length; i++)
+	for(var i = 0; i < args.length; i++) {
 		result.push(args[i].replace('@', hlCodePath ));
-
+	}
 	return result;
 };
-
 function loadCodeHightlight(){
 	SyntaxHighlighter.autoloader.apply(null, path(
-			'applescript			@shBrushAppleScript.js',
-			'actionscript3 as3	  @shBrushAS3.js',
-			'bash shell			 @shBrushBash.js',
-			'coldfusion cf		  @shBrushColdFusion.js',
-			'cpp c				  @shBrushCpp.js',
-			'c# c-sharp csharp	  @shBrushCSharp.js',
-			'css					@shBrushCss.js',
-			'delphi pascal		  @shBrushDelphi.js',
-			'diff patch pas		 @shBrushDiff.js',
-			'erl erlang			 @shBrushErlang.js',
-			'groovy				 @shBrushGroovy.js',
-			'java				   @shBrushJava.js',
-			'jfx javafx			 @shBrushJavaFX.js',
-			'js jscript javascript  @shBrushJScript.js',
-			'perl pl				@shBrushPerl.js',
-			'php					@shBrushPhp.js',
-			'text plain			 @shBrushPlain.js',
-			'py python			  @shBrushPython.js',
-			'ruby rails ror rb	  @shBrushRuby.js',
-			'sass scss			  @shBrushSass.js',
-			'scala				  @shBrushScala.js',
-			'sql					@shBrushSql.js',
-			'vb vbnet			   @shBrushVb.js',
-			'xml xhtml xslt html	@shBrushXml.js'
-		)
-	);
+				'applescript			@shBrushAppleScript.js',
+				'actionscript3 as3	  @shBrushAS3.js',
+				'bash shell			 @shBrushBash.js',
+				'coldfusion cf		  @shBrushColdFusion.js',
+				'cpp c				  @shBrushCpp.js',
+				'c# c-sharp csharp	  @shBrushCSharp.js',
+				'css					@shBrushCss.js',
+				'delphi pascal		  @shBrushDelphi.js',
+				'diff patch pas		 @shBrushDiff.js',
+				'erl erlang			 @shBrushErlang.js',
+				'groovy				 @shBrushGroovy.js',
+				'java				   @shBrushJava.js',
+				'jfx javafx			 @shBrushJavaFX.js',
+				'js jscript javascript  @shBrushJScript.js',
+				'perl pl				@shBrushPerl.js',
+				'php					@shBrushPhp.js',
+				'text plain			 @shBrushPlain.js',
+				'py python			  @shBrushPython.js',
+				'ruby rails ror rb	  @shBrushRuby.js',
+				'sass scss			  @shBrushSass.js',
+				'scala				  @shBrushScala.js',
+				'sql					@shBrushSql.js',
+				'vb vbnet			   @shBrushVb.js',
+				'xml xhtml xslt html	@shBrushXml.js'
+					)
+					);
 	SyntaxHighlighter.all();
 };
 
@@ -50,7 +48,8 @@ function loadCodeHightlight(){
 var padding = 5;
 function changeTocSize() {
 	var height = document.documentElement.clientHeight - 150;
-	$(".toc").height(height);
+	$("div.toc").height(height);
+	$("div.tocWrap").css('padding', padding + 'px');
 };
 
 /**
@@ -66,33 +65,36 @@ function closeAll() {
 	$('div.toc    ul').addClass('ul-index-close');
 	$('div.toc>ul ul').hide(300);
 };
+
 $(document).ready(function() {
-	loadCodeHightlight();
+	loadCodeHightlight(); // 启用语法高亮
 
-	if (window.innerWidth >= 480) {
-		// 目录标题
-		var toggler = $('<div class="toggler" title="点击展开/收起，Shift+Z 隐藏或打开">目录</div>');
-		toggler.click(function() { $('div.toc').slideToggle(300); });
+	// 目录的内容
+	var idxCtx = '<a href="javascript:openAll();">展开</a>&nbsp;<a href="javascript:closeAll();">收起</a>';
+	idxCtx = idxCtx + $('.toc').html();
+	$('.toc').remove();
+	// 目录内容区
+	var idxBody = '<div class="toc">' + idxCtx + '</div>';
+	// 目录标题
+	var idxTitle = '<div class="toggler" title="点击展开/收起，Shift+Z 隐藏或打开">目录</div>';
+	// 目录全部
+	var idxAll = '<div class="tocWrap">' + idxTitle + idxBody + '</div>';
 
-		// 目录
-		var toc = $('.toc');
-		toc.prepend('<a href="javascript:openAll();">展开</a>&nbsp;<a href="javascript:closeAll();">收起</a>');
-		toc.wrap('<div class="tocWrap">');
-		$('.tocWrap').prepend(toggler).delay(500).fadeTo(500, '0.25').hover(
-			function() { $(this).stop().fadeTo(300, '0.9'); },
-			function() { $(this).stop().fadeTo(300, '0.25'); }
+	/* 添加到内容中 */
+	$('.note-main').append(idxAll);
+	var toggler = $('.toggler');
+	var tocWrap = $('.tocWrap');
+	changeTocSize();         // 调整目录大小
+	openAll();               // 展开目录
+	$('div.toggler').click(function() { $('div.toc').toggle(); }); // 开关目录事件
+	$('div.tocWrap').show(); // 显示目录
+
+	/* jquery淡出效果 */
+	if (false) { // 只有jQuery生效
+		tocWrap.prepend(toggler).delay(500).fadeTo(500, '0.25').hover(
+				function() { $(this).stop().fadeTo(300, '0.9'); },
+				function() { $(this).stop().fadeTo(300, '0.25'); }
 		);
-		$(".tocWrap").css('padding',padding+'px');
-
-		changeTocSize();
-
-		$('html').keypress(function(e) {
-			if (e.shiftKey && (e.charCode || e.keyCode) == '90') {
-				e.preventDefault();
-				$('div.tocWrap').toggle(200);
-			}
-		});
-
-		openAll();
-	};
+	}
 });
+
